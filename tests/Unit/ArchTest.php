4ce2225a55da2a5b1447a70910ff3bc6943763c1
Diddyy <joshua.allen@outlook.com>
2025-09-21 18:10:24 +0100

    Switch ArchTest preset from `laravel` to `strict`

--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -3,10 +3,7 @@
 declare(strict_types=1);
 
 arch()->preset()->php();
-// Since Laravel Framework changed Model casting conventions from protected property $casts to protected function casts()
-// we need to wait for Pest PHP updates to support this.
-// arch()->preset()->strict();
-arch()->preset()->laravel();
+arch()->preset()->strict();
 arch()->preset()->security();
 
 arch('controllers')
