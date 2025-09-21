f1c9535ce10f27975feba94fd51fa356b41e12d5
Diddyy <joshua.allen@outlook.com>
2025-09-22 00:26:13 +0100

    Reorder imports in `welcome.tsx` for consistency.

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,6 +1,6 @@
+import { dashboard, login, register } from '@/routes';
 import { type SharedData } from '@/types';
 import { Head, Link, usePage } from '@inertiajs/react';
-import { dashboard, login, register } from '@/routes';
 
 export default function Welcome() {
     const { auth } = usePage<SharedData>().props;
