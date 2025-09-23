597629f01c9a544684b1eda62fc01031f309b1bc
Diddyy <joshua.allen@outlook.com>
2025-09-23 14:34:45 +0100

    Add data-test attribute to sidebar menu button for testing

--- resources/js/components/nav-user.tsx
+++ resources/js/components/nav-user.tsx
@@ -17,7 +17,11 @@ export function NavUser() {
             <SidebarMenuItem>
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
-                        <SidebarMenuButton size="lg" className="group text-sidebar-accent-foreground data-[state=open]:bg-sidebar-accent">
+                        <SidebarMenuButton
+                            size="lg"
+                            className="group text-sidebar-accent-foreground data-[state=open]:bg-sidebar-accent"
+                            data-test="sidebar-menu-button"
+                        >
                             <UserInfo user={auth.user} />
                             <ChevronsUpDown className="ml-auto size-4" />
                         </SidebarMenuButton>
